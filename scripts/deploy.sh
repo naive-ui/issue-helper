ba20a0da44b3b6193795143063f031276cf0cce9
xiezongchen <952937570@qq.com>
2021-06-15 23:30:06 +0800

    feat: add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git add -A
+git commit -m 'feat: deploy'
+
+git push -f git@github.com:naive-ui/issue-helper.git gh-pages
+
+# 部署到码云
+# git push -f git@gitee.com:naive-ui/issue-helper.git main
+
+cd -
